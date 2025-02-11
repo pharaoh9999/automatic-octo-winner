c2ddbab6a3582ad61c763b0653c116c801e89210
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2025-02-11 07:59:05 +0000

    kseurthj

--- includes/function.php
+++ includes/function.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 
+echo json_encode($_SERVER);
+//echo '<br/><br/>';
+//echo json_encode($_COOKIE);
+exit;
+
 if (!isset($_COOKIE['auth_token']) && !verify_access($_SERVER['PHP_SELF'])) {
     //header("Location: ./login.php");
     //exit;
-    if(isset($_SERVER['QUERY_STRING'])){
-        $query_str = $_SERVER['QUERY_STRING'];
-    }else{
-        $query_str = 'null';
-    }
     if ($_SERVER['SCRIPT_URL'] !== '/fingerprint.php' && $_SERVER['PHP_SELF'] !== '/kestrel/fingerprint.php') {
         header("Location: https://en.wikipedia.org/wiki/Mind_your_own_business?err=".$_SERVER['REQUEST_URI']);
         exit;
